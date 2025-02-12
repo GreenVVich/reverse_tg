adea0b02ec180ae34d8f6d1687c27b3c00539375
GreenVVich <96542151+GreenVVich@users.noreply.github.com>
2025-02-12 23:58:54 +0300

    New set tag (unique per set) + realize rules menu + rule fix + new rule

--- rules/__init__.py
+++ rules/__init__.py
@@ -1,5 +1,5 @@
 from rules.reason import REASON_RULE
 from rules.reversed_exorcism import REV_EXO_RULE
+from rules.rumors import RUMORS
 
-RULE_BOOK = [REASON_RULE, REV_EXO_RULE]
-
+RULE_BOOK = [REASON_RULE, REV_EXO_RULE, RUMORS]
