9c40e04ab2cd873bfbfe1ee4ac2b23c57a293835
GreenVVich <96542151+GreenVVich@users.noreply.github.com>
2025-05-05 23:18:31 +0300

    Added new rule

--- rules/__init__.py
+++ rules/__init__.py
@@ -2,5 +2,6 @@ from rules.reason import REASON_RULE
 from rules.reversed_exorcism import REV_EXO_RULE
 from rules.rumors import RUMORS
 from rules.you_fired import YOU_FIRED
+from rules.contact import YOU_CONTACTED
 
-RULE_BOOK = [REASON_RULE, REV_EXO_RULE, RUMORS, YOU_FIRED]
+RULE_BOOK = [REASON_RULE, REV_EXO_RULE, RUMORS, YOU_FIRED, YOU_CONTACTED]
