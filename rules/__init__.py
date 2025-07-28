5fdf13bf1e5322a1e3b7a78b0f41e044d30a9d52
GreenVVich <96542151+GreenVVich@users.noreply.github.com>
2025-07-29 01:41:06 +0300

    Added new rule

--- rules/__init__.py
+++ rules/__init__.py
@@ -4,5 +4,8 @@ from rules.rumors import RUMORS
 from rules.you_fired import YOU_FIRED
 from rules.contact import YOU_CONTACTED
 from rules.test import TEST
+from rules.birthdate import FELICITATION
 
-RULE_BOOK = [REASON_RULE, REV_EXO_RULE, RUMORS, YOU_FIRED, YOU_CONTACTED, TEST]
+# RULE_BOOK = [REASON_RULE, REV_EXO_RULE, RUMORS, YOU_FIRED, YOU_CONTACTED, TEST]
+RULE_BOOK = [REASON_RULE, REV_EXO_RULE, RUMORS, FELICITATION]
+
